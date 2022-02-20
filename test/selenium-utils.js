58cb8b5495655d1fe37a2a67353bc9672fec920f
Nikolay Leipi <nikolay.semykin@googlemail.com>
2022-02-20 16:56:25 +0100

    create selenium utils

---
+++ test/selenium-utils.js
@@ -0,0 +1,13 @@
+const { Condition } = require('selenium-webdriver')
+
+module.exports.promiseSettled = function (varName) {
+  return new Condition(`for promise ${varName} to resolve`, function (driver) {
+    return driver.executeAsyncScript(`
+var callback = arguments[arguments.length - 1]
+var promiseName = arguments[0]
+Promise.allSettled([window[promiseName]]).then(results => {
+  callback(results[0])
+})
+`, varName)
+  })
+}

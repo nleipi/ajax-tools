fbce60161a139c95b54f7b9522cc227a789a0170
Nikolay Leipi <nikolay.semykin@googlemail.com>
2026-03-09 13:29:43 +0100

    Check implicit and explicit contexts

--- tests/modes.spec.js
+++ tests/modes.spec.js
@@ -1,41 +1,51 @@
 import { expect } from '@playwright/test';
 import { test } from './fixtures'
 
-test.describe('loading', () => {
-  test('implicit', async ({ page, app }) => {
-    app.get('/test', (req, res) => {
-      const html = `
+test.describe('loading context', () => {
+  [
+    ['implicit', 'implicit'],
+    ['closest', 'closest-context'],
+    ['selector', 'global-context'],
+  ].forEach(([btnId, contextId]) => {
+    test(btnId, async ({ page, app }) => {
+      app.get('/test', (req, res) => {
+        const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <script type="module">
-      import ajt from './index.js'
-      window.ajt = ajt
-    </script>
+    <script type="module" src="./index.js"></script>
   </head>
   <body>
-    <a data-testid="btn" href="/submit" onclick="event.preventDefault(); ajt(event, this)">Update</a>
+    <a data-testid="implicit" href="/submit" data-ajt-trigger="click">Update</a>
+    <div class="my-context">dummy context to verify closest</div>
+    <div class="my-context" data-testid="closest-context">
+      <span>My context</span>
+      <a data-testid="closest" href="/submit" data-ajt-trigger="click" data-ajt-context-closest=".my-context">Update</a>
+    </div>
+    <a data-testid="selector" href="/submit" data-ajt-trigger="click" data-ajt-context-selector=".global-context">Update</a>
+    <div class="global-context" data-testid="global-context">Global context</div>
     <div id="test">Div before ajt call</div>
   </body>
 </html>
 `
-      res.send(html)
-    })
-    
-    const btnLocator = page.getByTestId('btn')
-    app.get('/submit', async (req, res) => {
-      await expect(btnLocator).toHaveAttribute('data-ajt-status', 'loading')
-      const html = `
+        res.send(html)
+      })
+      
+      const contextLocactor = page.getByTestId(contextId)
+      app.get('/submit', async (req, res) => {
+        await expect(contextLocactor).toHaveAttribute('data-ajt-status', 'loading')
+        const html = `
 <!DOCTYPE html>
 <div id="test" data-ajt-mode="replace">Div after ajt call</div>
 `
-      res.send(html)
+        res.send(html)
+      })
+      await page.goto('/test')
+      await expect(contextLocactor).not.toHaveAttribute('data-ajt-status', 'loading')
+      await page.getByTestId(btnId).click()
+      await expect(page.getByText('Div after ajt call')).not.toBeAttached()
+      await expect(contextLocactor).not.toHaveAttribute('data-ajt-status', 'loading')
+      await expect(page.getByText('Div after ajt call')).toBeAttached()
     })
-    await page.goto('/test')
-    await expect(btnLocator).not.toHaveAttribute('data-ajt-status', 'loading')
-    await page.getByTestId('btn').click()
-    await expect(page.getByText('Div after ajt call')).not.toBeAttached()
-    await expect(btnLocator).not.toHaveAttribute('data-ajt-status', 'loading')
-    await expect(page.getByText('Div after ajt call')).toBeAttached()
   })
 })

8e626e310a021167c769d7579be1570c4dc21368
Nikolay Leipi <nikolay.semykin@googlemail.com>
2026-03-10 17:20:16 +0100

    Add 'replace' and 'replaceContent' dom tests

--- app.js
+++ app.js
@@ -9,13 +9,23 @@ app.use(express.static('lib'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/a', (req, res) => {
+  res.append('Content-Security-Policy', "script-src 'nonce-foobar'")
   res.send(
 `
 <!DOCTYPE html>
 <html>
+  <head>
+    <script type="module" nonce="foobar">
+      window.nonce = "foobar"
+      new MutationObserver((mutations) => {
+        console.log(mutations)
+      }).observe(document, { childList: true, subtree: true })
+    </script>
+    <script type="module" nonce="foobar" src="./index.js"></script>
+  </head>
   <body>
     <main>
-      <form action="submit?test=42" method="get" onsubmit="event.preventDefault(); import('./submit-form.js').then(module => { module.default(event.target, event.submitter) })">
+      <form action="submit?test=42" method="get" data-ajt-trigger="submit">
         <input type="hidden" name="hidden_input" value="13">
         <input type="checkbox" name="ckb1" checked disabled>
         <input type="text" name="text_input" value="test">
@@ -23,6 +33,7 @@ app.get('/a', (req, res) => {
         <input type="submit" name="sbm">
         <input type="image" name="img">
       </form>
+      <div id="test" data-testid="el">Lorem <span>Div before ajt call<span>inner</span></span> ipsum</div>
     </main>
   </body>
 </html>
@@ -32,7 +43,10 @@ app.get('/a', (req, res) => {
 app.all('/submit', (req, res) => {
   console.log(req.query)
   console.log(req.is('application/x-www-form-urlencoded'))
-  res.sendStatus(200)
+  const html = `
+<div id="test" data-testid="newEl" data-ajt-mode="replace">Dolor <span>Div after ajt call</span> sit</div>
+`
+  res.send(html)
 })
 
 app.listen(port, () => {
---
+++ tests/dom.spec.js
@@ -0,0 +1,100 @@
+import { expect } from '@playwright/test';
+import { test } from './fixtures'
+
+test.describe('data-ajt-mode', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.addInitScript(() => {
+      window.handlersHistory = []
+      function pushAjtHistory(el, type) {
+        window.handlersHistory.push([type, el.outerHTML || el.textContent])
+      }
+      window.ajtElementAddedHandlers = [(el) => pushAjtHistory(el, 'added')]
+      window.ajtElementRemovedHandlers = [(el) => pushAjtHistory(el, 'removed')]
+      window.ajtElementPreAddHandlers = [(el) => pushAjtHistory(el, 'preadd')]
+    });
+  })
+
+  test('replace', async ({ page, app }) => {
+    app.get('/test', (req, res) => {
+      const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <script type="module" src="./index.js"></script>
+  </head>
+  <body>
+    <div id="test" data-testid="el">Div before ajt call</div>
+  </body>
+</html>
+`
+      res.send(html)
+    })
+    app.get('/submit', (req, res) => {
+      const html = `
+<div id="test" data-testid="el" data-ajt-mode="replace">Div after ajt call</div>
+`
+      res.send(html)
+    })
+    await page.goto('/test')
+    await page.getByTestId('el').evaluate((el) => window.oldElement = el)
+
+    await page.evaluate(() => window.ajt('/submit'))
+
+    await page.getByTestId('el').evaluate((el) => window.newElement = el)
+
+    expect(await page.evaluate(() => window.handlersHistory)).toEqual([
+      ['removed', '<div id="test" data-testid="el">Div before ajt call</div>'],
+      ['preadd', '<div id="test" data-testid="el" data-ajt-mode="replace">Div after ajt call</div>'],
+      ['added', '<div id="test" data-testid="el" data-ajt-mode="replace">Div after ajt call</div>'],
+    ])
+    await expect(page.getByText('Div after ajt call')).toBeAttached()
+    expect(await page.evaluate(() => {
+      return window.oldElement !== window.newElement
+    })).toBe(true)
+  })
+
+  test('replaceContent', async ({ page, app }) => {
+    app.get('/test', (req, res) => {
+      const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <script type="module" src="./index.js"></script>
+  </head>
+  <body>
+    <div id="test" data-testid="el">Lorem <span>Div before ajt call<span>inner</span></span> ipsum</div>
+  </body>
+</html>
+`
+      res.send(html)
+    })
+    app.get('/submit', (req, res) => {
+      const html = `
+<div id="test" data-testid="newEl" data-ajt-mode="replaceContent">Dolor <span>Div after ajt call</span> sit</div>
+`
+      res.send(html)
+    })
+    await page.goto('/test')
+    await page.getByTestId('el').evaluate((el) => window.oldElement = el)
+
+    await page.evaluate(() => window.ajt('/submit'))
+
+    await page.getByTestId('el').evaluate((el) => window.newElement = el)
+
+    expect(await page.evaluate(() => window.handlersHistory)).toEqual([
+      ['removed', 'Lorem '],
+      ['removed', '<span>Div before ajt call<span>inner</span></span>'],
+      ['removed', ' ipsum'],
+      ['preadd', 'Dolor '],
+      ['preadd', '<span>Div after ajt call</span>'],
+      ['preadd', ' sit'],
+      ['added', 'Dolor '],
+      ['added', '<span>Div after ajt call</span>'],
+      ['added', ' sit'],
+    ])
+    expect(await page.getByTestId('el').innerHTML()).toBe('Dolor <span>Div after ajt call</span> sit')
+    expect(await page.evaluate(() => {
+      return window.oldElement === window.newElement
+    })).toBe(true)
+  })
+})

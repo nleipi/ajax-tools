e18ccd19ecda6124db071f7f66f76d3bba378ee3
Nikolay Leipi <nikolay.semykin@googlemail.com>
2026-03-17 16:54:29 +0100

    add some basic examples

--- www/examples/urls.py
+++ www/examples/urls.py
@@ -6,8 +6,16 @@ app_name = "examples"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("simple-usecase/", include(([
-        path("", views.simple_usecase, name="Simple usecase"),
-        path("more/", views.simple_usecase_more, name="more"),
-    ], "simple_usecase"))),
+    path("replace/", include(([
+        path("", views.replace, name="replace"),
+        path("full/", views.replace_more, name="full"),
+    ], "replace"))),
+    # path("replace-content/", include(([
+    #     path("", views.replace_with_content, name="replaceWithContent"),
+    #     path("more/", views.replace_with_content_more, name="more"),
+    # ], "replace_content"))),
+    path("replace-with-content/", include(([
+        path("", views.replace_with_content, name="replaceWithContent"),
+        path("more/", views.replace_with_content_more, name="more"),
+    ], "replace_with_content"))),
 ]
--- www/examples/views.py
+++ www/examples/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpRequest
+from inspect import getdoc
 from django.urls import get_resolver, URLResolver
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils.lorem_ipsum import paragraphs
 
 from . import urls
 
@@ -14,7 +16,8 @@ def index(request):
                 if str(pattern.pattern) == '':
                     examples.append({
                         'full_name': f"{url_name}:{pattern.name}",
-                        'name': pattern.name
+                        'name': pattern.name,
+                        'description': getdoc(pattern.callback),
                     })
 
 
@@ -22,8 +25,40 @@ def index(request):
         'examples': examples
     })
 
-def simple_usecase(request):
-    return render(request, "examples/simple_usecase/index.html", {})
+def replace(request):
+    """
+    Simple example of data-ajt-mode="replace".
+    """
+    return render(request, "examples/replace/index.html", {
+        'text': paragraphs(1),
+        'show_more': True,
+    })
 
-def simple_usecase_more(request):
-    return render(request, "examples/simple_usecase/more.html", {})
+def replace_more(request: HttpRequest):
+    print(request.headers)
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        template = "examples/replace/text.html"
+    else:
+        template = "examples/replace/index.html"
+    return render(request, template, {
+        'text': paragraphs(5)
+    })
+
+def replace_with_content(request):
+    """
+    Simple example of data-ajt-mode="replaceWithContent"
+    """
+    return render(request, "examples/replace_with_content/index.html", {
+        'text': paragraphs(1),
+        'show_more': True,
+    })
+
+def replace_with_content_more(request):
+    print(request.headers)
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        template = "examples/replace_with_content/more.html"
+    else:
+        template = "examples/replace_with_content/index.html"
+    return render(request, template, {
+        'text': paragraphs(5)[2:]
+    })

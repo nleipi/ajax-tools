c93e2c91d99a131359ffaf0bad1c4088bdfbca81
Lucky Salmon <lucky-salmon@tutanota.com>
2026-04-23 16:38:26 +0200

    Add test data WIP

--- www/examples/models.py
+++ www/examples/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    sku = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products')
+
+    def __str__(self):
+        return self.name
